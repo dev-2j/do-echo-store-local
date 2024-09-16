6ebd871fb4a3ae71b176f35ab34b35a451b28370
dev-jack <dev-2j@hotmail.com>
2024-09-16 23:51:43 +0700

    ok

--- action/x-actions.go
+++ action/x-actions.go
@@ -7,5 +7,9 @@ import (
 )
 
 func XAction(c echo.Context) error {
+	/// test
+	//git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
 	return c.JSON(http.StatusOK, echo.Map{"message": "Hello, World!"})
+
 }
